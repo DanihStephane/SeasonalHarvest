27390bffec5ed82773542d068e93e0bd3890802c
DanihStephane <92985744+DanihStephane@users.noreply.github.com>
2025-01-06 15:38:09 +0300

    Update regions.ts

--- src/data/regions.ts
+++ src/data/regions.ts
@@ -1,19 +1,118 @@
 export interface Region {
   id: string;
   name: string;
-  coordinates: string; // Coordonnées SVG pour le path
+  coordinates: string;
 }
 
 export const regions: Region[] = [
   {
     id: 'diana',
     name: 'Diana',
-    coordinates: 'M 150,100 L 200,100 L 200,150 L 150,150 Z'
+    coordinates: 'M 200,50 L 240,40 L 260,60 L 250,90 L 220,100 L 190,80 Z'
   },
   {
     id: 'sava',
     name: 'Sava',
-    coordinates: 'M 200,100 L 250,100 L 250,150 L 200,150 Z'
+    coordinates: 'M 260,60 L 290,50 L 310,70 L 300,100 L 270,110 L 250,90 Z'
   },
-  // Ajoutez plus de régions avec leurs coordonnées SVG
+  {
+    id: 'sofia',
+    name: 'Sofia',
+    coordinates: 'M 190,80 L 220,100 L 250,90 L 270,110 L 260,140 L 220,150 L 180,130 Z'
+  },
+  {
+    id: 'boeny',
+    name: 'Boeny',
+    coordinates: 'M 180,130 L 220,150 L 210,180 L 170,190 L 150,170 Z'
+  },
+  {
+    id: 'betsiboka',
+    name: 'Betsiboka',
+    coordinates: 'M 220,150 L 260,140 L 250,180 L 210,180 Z'
+  },
+  {
+    id: 'melaky',
+    name: 'Melaky',
+    coordinates: 'M 150,170 L 170,190 L 160,230 L 130,240 L 120,210 Z'
+  },
+  {
+    id: 'alaotra-mangoro',
+    name: 'Alaotra-Mangoro',
+    coordinates: 'M 260,140 L 300,130 L 290,170 L 250,180 Z'
+  },
+  {
+    id: 'analanjirofo',
+    name: 'Analanjirofo',
+    coordinates: 'M 300,130 L 330,120 L 320,160 L 290,170 Z'
+  },
+  {
+    id: 'atsinanana',
+    name: 'Atsinanana',
+    coordinates: 'M 290,170 L 320,160 L 310,200 L 280,210 Z'
+  },
+  {
+    id: 'bongolava',
+    name: 'Bongolava',
+    coordinates: 'M 170,190 L 210,180 L 200,220 L 160,230 Z'
+  },
+  {
+    id: 'vakinankaratra',
+    name: 'Vakinankaratra',
+    coordinates: 'M 200,220 L 240,210 L 230,250 L 190,260 Z'
+  },
+  {
+    id: 'haute-matsiatra',
+    name: 'Haute Matsiatra',
+    coordinates: 'M 190,260 L 230,250 L 220,290 L 180,300 Z'
+  },
+  {
+    id: 'ihorombe',
+    name: 'Ihorombe',
+    coordinates: 'M 180,300 L 220,290 L 210,330 L 170,340 Z'
+  },
+  {
+    id: 'atsimo-andrefana',
+    name: 'Atsimo Andrefana',
+    coordinates: 'M 130,320 L 170,340 L 160,380 L 120,360 Z'
+  },
+  {
+    id: 'menabe',
+    name: 'Menabe',
+    coordinates: 'M 130,240 L 170,260 L 160,300 L 120,280 Z'
+  },
+  {
+    id: 'vatovavy',
+    name: 'Vatovavy',
+    coordinates: 'M 230,250 L 270,240 L 260,280 L 220,290 Z'
+  },
+  {
+    id: 'fitovinany',
+    name: 'Fitovinany',
+    coordinates: 'M 270,240 L 310,230 L 300,270 L 260,280 Z'
+  },
+  {
+    id: 'atsimo-atsinanana',
+    name: 'Atsimo Atsinanana',
+    coordinates: 'M 220,290 L 260,280 L 250,320 L 210,330 Z'
+  },
+  {
+    id: 'androy',
+    name: 'Androy',
+    coordinates: 'M 170,340 L 210,330 L 200,370 L 160,380 Z'
+  },
+  {
+    id: 'anosy',
+    name: 'Anosy',
+    coordinates: 'M 210,330 L 250,320 L 240,360 L 200,370 Z'
+  },
+  {
+    id: 'analamanga',
+    name: 'Analamanga',
+    coordinates: 'M 210,180 L 250,180 L 240,210 L 200,220 Z'
+  },
+  {
+    id: 'itasy',
+    name: 'Itasy',
+    coordinates: 'M 240,210 L 280,210 L 270,240 L 230,250 Z'
+  }
 ];
