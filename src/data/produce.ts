1ffe2d1812258f0493715c6adb3d92b299621be3
DanihStephane <92985744+DanihStephane@users.noreply.github.com>
2025-01-06 15:39:00 +0300

    Update produce.ts

--- src/data/produce.ts
+++ src/data/produce.ts
@@ -12,21 +12,69 @@ export const produceData: Produce[] = [
     name: 'Mangue',
     type: 'fruit',
     seasons: ['spring', 'summer'],
-    regions: ['Diana', 'Sava', 'Boeny']
+    regions: ['diana', 'sava', 'boeny', 'sofia']
   },
   {
     id: 2,
     name: 'Litchi',
     type: 'fruit',
     seasons: ['summer'],
-    regions: ['Atsinanana', 'Analanjirofo']
+    regions: ['atsinanana', 'analanjirofo', 'vatovavy']
   },
   {
     id: 3,
     name: 'Manioc',
     type: 'legume',
     seasons: ['spring', 'summer', 'autumn', 'winter'],
-    regions: ['Haute Matsiatra', 'Vakinankaratra']
+    regions: ['haute-matsiatra', 'vakinankaratra', 'itasy']
   },
-  // Ajoutez plus de données factices ici
+  {
+    id: 4,
+    name: 'Vanille',
+    type: 'fruit',
+    seasons: ['winter', 'spring'],
+    regions: ['sava', 'analanjirofo']
+  },
+  {
+    id: 5,
+    name: 'Ananas',
+    type: 'fruit',
+    seasons: ['summer', 'autumn'],
+    regions: ['atsinanana', 'analanjirofo', 'vatovavy']
+  },
+  {
+    id: 6,
+    name: 'Patate Douce',
+    type: 'legume',
+    seasons: ['autumn', 'winter'],
+    regions: ['vakinankaratra', 'haute-matsiatra', 'ihorombe']
+  },
+  {
+    id: 7,
+    name: 'Banane',
+    type: 'fruit',
+    seasons: ['spring', 'summer', 'autumn', 'winter'],
+    regions: ['diana', 'sava', 'analanjirofo', 'atsinanana']
+  },
+  {
+    id: 8,
+    name: 'Tomate',
+    type: 'legume',
+    seasons: ['spring', 'summer'],
+    regions: ['analamanga', 'vakinankaratra', 'itasy']
+  },
+  {
+    id: 9,
+    name: 'Avocat',
+    type: 'fruit',
+    seasons: ['winter', 'spring'],
+    regions: ['atsinanana', 'analanjirofo', 'vatovavy']
+  },
+  {
+    id: 10,
+    name: 'Carotte',
+    type: 'legume',
+    seasons: ['autumn', 'winter'],
+    regions: ['vakinankaratra', 'haute-matsiatra', 'analamanga']
+  }
 ];
